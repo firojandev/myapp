1bdede1b660ee511f9c5c4e3b1b61b9467ab020d
altaf <altaf@squaregroup.com>
2021-06-01 15:52:54 +0600

    router added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
 
-app.use(express.json())
+app.use(express.json());
+
+const router = express.Router();
+
+app.use(router);
 
 app.get('/',(req,res) => {
     res.send('call successfully:'+req.method);
+});
+
+app.post('/',(req,res) => {
+    console.log(req.body);
+    res.send('Hello Mrs:'+req.body.title);
 });
 
 app.listen(3000,() => {
