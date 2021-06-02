e75a186e71c495ed693877a6bd17c7d6c2ed8ae6
altaf <altaf@squaregroup.com>
2021-06-02 12:07:00 +0600

    CRUD operation api done

--- app/controllers/customer.controller.js
+++ app/controllers/customer.controller.js
@@ -42,17 +42,71 @@ exports.findAllCustomers = (req, res) => {
 
 exports.findOne = (req, res) => {
     Customer.findById(req.params.customerId, (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found Customer with id ${req.params.customerId}.`
-          });
-        } else {
-          res.status(500).send({
-            message: "Error retrieving Customer with id " + req.params.customerId
-          });
-        }
-      } else res.send(data);
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Customer with id ${req.params.customerId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving Customer with id " + req.params.customerId
+                });
+            }
+        } else res.send(data);
     });
-  };
+};
+
+exports.update = (req, res) => {
+    // Validate Request
+    if (!req.body) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+
+    Customer.updateById(
+        req.params.customerId,
+        new Customer(req.body),
+        (err, data) => {
+            if (err) {
+                if (err.kind === "not_found") {
+                    res.status(404).send({
+                        message: `Not found Customer with id ${req.params.customerId}.`
+                    });
+                } else {
+                    res.status(500).send({
+                        message: "Error updating Customer with id " + req.params.customerId
+                    });
+                }
+            } else res.send(data);
+        }
+    );
+};
+
+exports.delete = (req, res) => {
+    Customer.remove(req.params.customerId, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found Customer with id ${req.params.customerId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Could not delete Customer with id " + req.params.customerId
+                });
+            }
+        } else res.send({ message: `Customer was deleted successfully!` });
+    });
+};
+
+exports.deleteAll = (req, res) => {
+    Customer.removeAll((err, data) => {
+        if (err)
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while removing all customers."
+            });
+        else res.send({ message: `All Customers were deleted successfully!` });
+    });
+};
 
--- app/routes/customer.route.js
+++ app/routes/customer.route.js
@@ -2,12 +2,23 @@ const customersController = require('../controllers/customer.controller');
 
 const customerRoute = (app) => {
 
+    //Get all customers
     app.get('/customers', customersController.findAllCustomers);
+
+    //Create a new customer
     app.post('/customers', customersController.createCustomer);
 
     // Retrieve a single Customer with customerId
     app.get("/customers/:customerId", customersController.findOne);
 
+    // Update a Customer with customerId
+    app.put("/customers/:customerId", customersController.update);
+
+    // Delete a Customer with customerId
+    app.delete("/customers/:customerId", customersController.delete);
+
+    // Delete all Customers
+    app.delete("/customers", customersController.deleteAll);
 
 }
 
