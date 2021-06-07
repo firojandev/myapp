ebe0c2e93664aa4cf89d927888868dd86cbfae51
altaf <altaf@squaregroup.com>
2021-06-07 09:21:14 +0600

    ware middleware added

--- app/modules/warehouse/warehouse.route.js
+++ app/modules/warehouse/warehouse.route.js
@@ -1,20 +1,21 @@
+const verifyToken = require('../../middleware/VerifyToken');
 const whc = require('./warehouse.controller');
 
 const warehouseRoute = (app) => {
     // Retrieve all Warehouse
-    app.get("/warehouses",whc.findAll);
+    app.get("/warehouses",verifyToken, whc.findAll);
 
     // Retrieve a Warehouse
-    app.get("/warehouses/:id",whc.findById);
+    app.get("/warehouses/:id",verifyToken,whc.findById);
 
     // Create a new Warehouse
-    app.post("/warehouses",whc.create);
+    app.post("/warehouses",verifyToken,whc.create);
 
     // Create a new Warehouse
-    app.put("/warehouses/:id",whc.updateWarehouse);
+    app.put("/warehouses/:id",verifyToken,whc.updateWarehouse);
 
     // delete Warehouse
-    app.delete("/warehouses/:id",whc.deleteWarehouse);
+    app.delete("/warehouses/:id",verifyToken,whc.deleteWarehouse);
 }
 
 module.exports = warehouseRoute;
